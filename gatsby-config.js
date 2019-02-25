30f574a768218116d11b52876ec3a471859d8b14
max-jellycat <max@jellycat.fr>
2019-02-26 00:07:46 +0100

    added helmet and SEO component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,11 @@
 module.exports = {
+  siteMetadata: {
+    title: "Salt & Paper",
+    titleTemplate: "%s",
+    description: "Succession of words written with salty ink.",
+    url: "salt-and-paper.fr",
+    image: "",
+    twitterUsername: "@max-jellycat",
+  },
   plugins: [`gatsby-plugin-netlify-cms`],
 }
